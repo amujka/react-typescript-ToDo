b0e9df7584260d513a7b244398de92db08404c3d
amujka <mujkanovic.alen091@gmail.com>
2021-06-21 11:23:07 +0200

    -

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import Todo from '../model/todo'
-
 const TodoItem:React.FC<{todo:Todo,deleteTodoHandler:(id:number)=>void}> = ({todo,deleteTodoHandler}) => {
-
- 
     return (
         <li className="item"     onClick={()=>{deleteTodoHandler(todo.id)}}>
-           
            {todo.name} 
-
-          
-    
         </li>
     )
 }
-
 export default TodoItem
